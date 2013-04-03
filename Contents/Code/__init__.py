dba6c753f73e05b32d358fbfeae327339ccfe83a
sander1 <sander@plexapp.com>
2013-04-03 18:51:30 +0200

    Use hosted resources

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,65 +1,61 @@
-VEVO_TITLE_INFO             = 'http://videoplayer.vevo.com/VideoService/AuthenticateVideo?isrc=%s&authToken=%s&domain=http://www.vevo.com'
-VEVO_API_URL                = 'http://api.vevo.com/mobile/v1/%s/list.json'
-VEVO_URL                    = 'http://www.vevo.com'
-VIDEO_URL                   = 'http://www.vevo.com/watch/%s/%s/%s'
-ARTIST_VIDEOS_URL           = 'http://www.vevo.com/data/artist/%s'
-SEARCH_URL                  = 'http://api.vevo.com/mobile/v1/lookahead.json?q=%s&fullItems=true&newSearch=yes'
+VEVO_TITLE_INFO = 'http://videoplayer.vevo.com/VideoService/AuthenticateVideo?isrc=%s&authToken=%s&domain=http://www.vevo.com'
+VEVO_API_URL = 'http://api.vevo.com/mobile/v1/%s/list.json'
+VEVO_URL = 'http://www.vevo.com'
+VIDEO_URL = 'http://www.vevo.com/watch/%s/%s/%s'
+ARTIST_VIDEOS_URL = 'http://www.vevo.com/data/artist/%s'
+SEARCH_URL = 'http://api.vevo.com/mobile/v1/lookahead.json?q=%s&fullItems=true&newSearch=yes'
 
 VIDEO_PREFIX = "/video/vevo"
 NAME = 'Vevo'
-ART  = 'art-default.jpg'
-ICON = 'icon-default.png'
-SEARCHICON = 'icon-search.png'
 
 ####################################################################################################
 def Start():
 
-    Plugin.AddPrefixHandler(VIDEO_PREFIX, MainMenu, NAME, ICON, ART)
+    Plugin.AddPrefixHandler(VIDEO_PREFIX, MainMenu, NAME)
 
-    Plugin.AddViewGroup("InfoList", viewMode="InfoList", mediaType="items")
-
-    ObjectContainer.art = R(ART)
     ObjectContainer.title1 = NAME
-    DirectoryObject.thumb = R(ICON)
-    
     HTTP.CacheTime = CACHE_1HOUR
 
 ####################################################################################################
-#Navigation
 def MainMenu():
 
     oc = ObjectContainer()
     oc.add(DirectoryObject(key=Callback(VideosSubMenu), title = "Videos"))
     oc.add(DirectoryObject(key=Callback(ArtistsSubMenu), title = "Artists"))
     oc.add(DirectoryObject(key=Callback(AllGenresSubMenu), title = "Genres"))
-    oc.add(DirectoryObject(key=Callback(SearchMenu), title="Search", thumb=R(SEARCHICON)))
-    
+    oc.add(DirectoryObject(key=Callback(SearchMenu), title="Search"))
+
     return oc
 
 ####################################################################################################
 def SearchMenu():
+
     oc = ObjectContainer(title2="Search")
-    oc.add(InputDirectoryObject(key=Callback(ArtistSearch), title="Search for Artists", prompt="Search for...",
-        thumb=R(SEARCHICON)))
-    oc.add(SearchDirectoryObject(identifier="com.plexapp.plugins.vevo", title="Search for Videos", prompt="Search for...",
-        thumb=R(SEARCHICON)))
+    oc.add(InputDirectoryObject(key=Callback(ArtistSearch), title="Search for Artists", prompt="Search for...", thumb=R(SEARCHICON)))
+    oc.add(SearchDirectoryObject(identifier="com.plexapp.plugins.vevo", title="Search for Videos", prompt="Search for...", thumb=R(SEARCHICON)))
+
     return oc
 
 ####################################################################################################
 def ArtistSearch(query):
+
     oc = ObjectContainer(title2='Search Results')
     results = JSON.ObjectFromURL(SEARCH_URL % String.Quote(query))['Artists']
+
     for artist in results:
         title = artist['name']
         thumb = artist['img']
-        oc.add(DirectoryObject(key=Callback(ArtistVideoListing, name=title, urlsafe_name=artist['urlKey']), title=title,
-            thumb=Resource.ContentsOfURLWithFallback(url=thumb, fallback='icon-default.png')))
-    if len(oc) == 0:
+
+        oc.add(DirectoryObject(key=Callback(ArtistVideoListing, name=title, urlsafe_name=artist['urlKey']), title=title, thumb=Resource.ContentsOfURLWithFallback(url=thumb)))
+
+    if len(oc) < 1:
         return ObjectContainer(header=NAME, message="Sorry. No results found")
+
     return oc
 
 ####################################################################################################
 def VideosSubMenu(title=None, genre=None):
+
     if title:
         oc = ObjectContainer(title1="Videos", title2=title)
     else:
@@ -70,11 +66,12 @@ def VideosSubMenu(title=None, genre=None):
     oc.add(DirectoryObject(key=Callback(VideoListing, title="Most Viewed this week", request="MostViewedThisWeek", genres=genre), title="Most Viewed this week"))
     oc.add(DirectoryObject(key=Callback(VideoListing, title="Most Viewed this month", request="MostViewedThisMonth", genres=genre), title="Most Viewed this month"))
     oc.add(DirectoryObject(key=Callback(VideoListing, title="Most Viewed of All Time", request="MostViewedAllTime", genres=genre), title="Most Viewed of All Time"))
-    
+
     return oc
 
 ####################################################################################################
 def ArtistsSubMenu(title=None, genre=None):
+
     if title:
         oc = ObjectContainer(title1="Videos", title2=title)
     else:
@@ -85,34 +82,40 @@ def ArtistsSubMenu(title=None, genre=None):
     oc.add(DirectoryObject(key=Callback(ArtistListing, title="Most Viewed this week", request="MostViewedThisWeek", genres=genre), title="Most Viewed this week"))
     oc.add(DirectoryObject(key=Callback(ArtistListing, title="Most Viewed this month", request="MostViewedThisMonth", genres=genre), title="Most Viewed this month"))
     oc.add(DirectoryObject(key=Callback(ArtistListing, title="Most Viewed of All Time", request="MostViewedAllTime", genres=genre), title="Most Viewed of All Time"))
-    
+
     return oc
 
 ####################################################################################################
 def AllGenresSubMenu():
+
     oc = ObjectContainer()
     genres = JSON.ObjectFromURL(VEVO_API_URL % 'genre')['result']
+
     for genre in genres:
         oc.add(DirectoryObject(key=Callback(GenreSubMenu, title=genre['Value'], genre=genre['Key']), title=genre['Value']))
+
     return oc
 
 ####################################################################################################
 def GenreSubMenu(title, genre):
+
     oc = ObjectContainer(title2=title)
-    
     oc.add(DirectoryObject(key=Callback(VideosSubMenu, title=title, genre=genre), title="Videos"))
     oc.add(DirectoryObject(key=Callback(ArtistsSubMenu, title=title, genre=genre), title="Artists"))
-    
+
     return oc
 
 ####################################################################################################
 def VideoListing(title, group='video', request=None, genres=None, offset=0):
+
     if offset != 0:
         oc = ObjectContainer(title2=title, replace_parent=True)
         oc.add(DirectoryObject(key=Callback(VideoListing, title=title, request=request, genres=genres, offset=(offset-20)), title="Previous"))
     else:
         oc = ObjectContainer(title2=title)
+
     results = API_Call(group, request, genres, offset)['result']
+
     for result in results:
         video_title = result['title']
         thumb = result['image_url']
@@ -121,6 +124,7 @@ def VideoListing(title, group='video', request=None, genres=None, offset=0):
         featured_artists = result['artists_featured']
         url = VIDEO_URL % (result['artists_main'][0]['url_safename'], result['url_safe_title'], result['isrc'])
         summary = ''
+
         if len(artists) == 1:
             summary = 'Artist: %s' % artists[0]['name']
         elif len(artists) > 1:
@@ -130,20 +134,26 @@ def VideoListing(title, group='video', request=None, genres=None, offset=0):
             summary = summary.strip(', ')
         else:
             pass
+
         if len(featured_artists) > 0:
             summary = summary + '\nFeaturing: '
+
             for featured in featured_artists:
                 summary = summary + featured['name'] + ', '
             summary = summary.strip(', ')
-        oc.add(VideoClipObject(url=url, title=video_title, summary=summary, duration=duration,
-            thumb=Resource.ContentsOfURLWithFallback(url=thumb, fallback='icon-default.png')))
+
+        oc.add(VideoClipObject(url=url, title=video_title, summary=summary, duration=duration, thumb=Resource.ContentsOfURLWithFallback(url=thumb)))
+
     oc.add(DirectoryObject(key=Callback(VideoListing, title=title, request=request, genres=genres, offset=(offset+20)), title="More"))
-    if len(oc) == 0:
+
+    if len(oc) < 1:
         return ObjectContainer(header=NAME, message="No entries found.")
+
     return oc
 
 ####################################################################################################
 def ArtistListing(title, group='artist', request=None, genres=None, offset=0):
+
     if offset != 0:
         oc = ObjectContainer(title2=title, replace_parent=True)
         oc.add(DirectoryObject(key=Callback(ArtistListing, title=title, request=request, genres=genres, offset=(offset-20)), title="Previous"))
@@ -155,31 +165,38 @@ def ArtistListing(title, group='artist', request=None, genres=None, offset=0):
     for artist in results:
         name = artist['name']
         thumb = artist['image_url'] + '?width=512&height=512&crop=auto'
-        oc.add(DirectoryObject(key=Callback(ArtistVideoListing, name=name, urlsafe_name=artist['url_safename']), title=name,
-            thumb=Resource.ContentsOfURLWithFallback(url=thumb, fallback='icon-default.png')))
+
+        oc.add(DirectoryObject(key=Callback(ArtistVideoListing, name=name, urlsafe_name=artist['url_safename']), title=name, thumb=Resource.ContentsOfURLWithFallback(url=thumb)))
 
     oc.add(DirectoryObject(key=Callback(ArtistListing, title=title, request=request, genres=genres, offset=(offset+20)), title="More"))
 
-    if len(oc) == 0:
+    if len(oc) < 1:
         return ObjectContainer(header=NAME, message="No entries found.")
+
     return oc
 
 ####################################################################################################
 def ArtistVideoListing(name, urlsafe_name):
+
     oc = ObjectContainer(title2=name)
     videos = JSON.ObjectFromURL(ARTIST_VIDEOS_URL % urlsafe_name)['Videos']
+
     for video in videos:
         title = video['title']
         thumb = video['img']
         url = VEVO_URL + video['url']
-        oc.add(VideoClipObject(url=url, title=title, thumb=Resource.ContentsOfURLWithFallback(url=thumb, fallback='icon-default.png')))
-    if len(oc) == 0:
+        oc.add(VideoClipObject(url=url, title=title, thumb=Resource.ContentsOfURLWithFallback(url=thumb)))
+
+    if len(oc) < 1:
         return ObjectContainer(header=NAME, message="No entries found.")
+
     return oc
-    
+
 ####################################################################################################
 def API_Call(group=None, request=None, genres=None, offset=None):
+
     params = ''
+
     if request:
         params = BuildParams(params, "order=%s" % request)
     if offset:
@@ -188,14 +205,16 @@ def API_Call(group=None, request=None, genres=None, offset=None):
         params = BuildParams(params, "max=%s" % '25')
     if genres:
         params = BuildParams(params, "genres=%s" % genres)
-        
+
     url = (VEVO_API_URL % group) + params
     return JSON.ObjectFromURL(url) 
 
 ####################################################################################################
 def BuildParams(params, new_param):
+
     if not params.startswith('?'):
         params = '?' + params
     if params[-1] != '?':
         params = params + '&'
+
     return (params + new_param)
